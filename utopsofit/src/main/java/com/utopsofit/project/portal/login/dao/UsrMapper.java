8f2ff4f90ec7dc3040e0dfb3ae222cc2832bf785
utopsoft <utopsoft@DESKTOP-SNIJLFL>
2026-03-05 11:02:44 +0900

    배치

--- utopsofit/src/main/java/com/utopsofit/project/portal/login/dao/UsrMapper.java
+++ utopsofit/src/main/java/com/utopsofit/project/portal/login/dao/UsrMapper.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Param;
 
 import com.utopsofit.project.portal.login.domain.Usr;
 
+import java.util.List;
+
 /**
  * 사용자 MyBatis Mapper
  */
@@ -18,4 +20,10 @@ public interface UsrMapper {
     int updateLoginFailCnt(@Param("usrId") String usrId, @Param("loginFailCnt") int loginFailCnt);
 
     int updateLockYn(@Param("usrId") String usrId, @Param("lockYn") String lockYn);
+
+    /** 마지막 로그인이 inactiveDays일 이상 경과한 미잠금 사용자 조회 (배치용) */
+    List<Usr> selectInactiveUsrs(@Param("inactiveDays") int inactiveDays);
+
+    /** 마지막 로그인이 inactiveDays일 이상 경과한 사용자 일괄 잠금 처리 (배치용) */
+    int lockInactiveUsrs(@Param("inactiveDays") int inactiveDays);
 }
